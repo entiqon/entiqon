5ae2c6966e932b0a636a733f095246d90e06056c
Isidro Lopez <isidro.lopezg@live.com>
2025-05-17 18:58:13 -0400

    chore: remove experimental builder and dialect test files outside internal/

--- dialect/dialect.go
+++
@@ -1,8 +0,0 @@
-package dialect
-
-type Dialect interface {
-	// EscapeIdentifier like table/column names
-	EscapeIdentifier(identifier string) string
-	// Placeholder syntax: ?, $1, :name, etc.
-	Placeholder(index int) string
-}
